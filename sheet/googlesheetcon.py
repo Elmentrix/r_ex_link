3d2e9878de44e6324e803d8ad271e052a59f10f2
Emmanuel Laryea <laryeaonyx@gmail.com>
2024-02-19 02:34:38 +0000

    2fworked

--- sheet/googlesheetcon.py
+++ sheet/googlesheetcon.py
@@ -14,8 +14,66 @@ sheet = worksheet.get_all_values()
 
 print("PERFECT TEAM WORK NEED GOOD INPUT")
 
-# Print all files
-for rows in sheet:
-    print(rows)
-    # for name in rows:
-    #     print(name)
+# selection function to help test the various functionalities of the code
+def selection():
+    selected_number = int(input(
+        """
+        Make a selection on the various options below based on what you want to do on our platform.
+
+        Options:
+        1. View Data
+        2. Add Data
+        3. Delete
+        
+        """
+    ))
+    return selected_number
+
+
+# data input function
+def input_data():
+    name = str(input("Name: "))
+    age = int(input("Age: "))
+    image_link = str(input("Image: "))
+    return name, age, image_link
+
+# Main code functions
+# function for adding data into the spreadsheet
+def add_data(name, age, inage_link):
+    update_data = [name, age, image_link]
+
+    # appending to the file
+    worksheet.append_row(update_data)
+
+
+# function for fetching data from spreadsheet
+def fetch_data():
+# Print all data in files
+    print("Total: ", sheet.count())
+    for rows in sheet[1:]:
+        print(rows[0])
+
+
+# function for deleting a particular dataset from the spreadsheet
+def delete_data():
+    # this will print out the list with numbers, then the user make selection of the number which comes as the array
+    print("working on the delete function")
+
+
+
+# calling for the functions above
+num = selection()
+if num == 2:
+    name, age = input_data()
+    add_data(name, age)
+
+elif num == 1:
+    fetch_data()
+
+elif num ==  3:
+    delete_data()
+
+
+
+
+
